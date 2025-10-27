b7fd8870e66e0bc42f658dcabe7f20994df7b2e2
Andriy Zavada <johnhommer@gmail.com>
2025-10-28 01:39:56 +0200

    fixups

--- src/leveled_cdb.erl
+++ src/leveled_cdb.erl
@@ -493,7 +493,8 @@ starting({call, From}, {open_writer, Filename}, State) ->
     {next_state, writer, State0, [{reply, From, ok}, hibernate]};
 starting({call, From}, {open_reader, Filename}, State) ->
     leveled_log:save(State#state.log_options),
-    leveled_monitor:add_stat(State#state.monitor, {n_active_journal_files_update, +1}),
+    {Monitor, _} = State#state.monitor,
+    leveled_monitor:add_stat(Monitor, {n_active_journal_files_update, +1}),
     leveled_log:log(cdb02, [Filename]),
     {Handle, Index, LastKey} = open_for_readonly(Filename, false),
     State0 = State#state{
@@ -505,7 +506,8 @@ starting({call, From}, {open_reader, Filename}, State) ->
     {next_state, reader, State0, [{reply, From, ok}, hibernate]};
 starting({call, From}, {open_reader, Filename, LastKey}, State) ->
     leveled_log:save(State#state.log_options),
-    leveled_monitor:add_stat(State#state.monitor, {n_active_journal_files_update, +1}),
+    {Monitor, _} = State#state.monitor,
+    leveled_monitor:add_stat(Monitor, {n_active_journal_files_update, +1}),
     leveled_log:log(cdb02, [Filename]),
     {Handle, Index, LastKey} = open_for_readonly(Filename, LastKey),
     State0 = State#state{
@@ -882,7 +884,8 @@ delete_pending(
 ) when
     ?IS_DEF(FN), ?IS_DEF(IO)
 ->
-    leveled_monitor:add_stat(State#state.monitor, {n_active_journal_files_update, -1}),
+    {Monitor, _} = State#state.monitor,
+    leveled_monitor:add_stat(Monitor, {n_active_journal_files_update, -1}),
     leveled_log:log(cdb04, [FN, State#state.delete_point]),
     close_pendingdelete(IO, FN, State#state.waste_path),
     {stop, normal};
@@ -909,7 +912,8 @@ delete_pending(
                 ),
             {keep_state_and_data, [?DELETE_TIMEOUT]};
         false ->
-            leveled_monitor:add_stat(State#state.monitor, {n_active_journal_files_update, -1}),
+            {Monitor, _} = State#state.monitor,
+            leveled_monitor:add_stat(Monitor, {n_active_journal_files_update, -1}),
             leveled_log:log(cdb04, [FN, ManSQN]),
             close_pendingdelete(IO, FN, State#state.waste_path),
             {stop, normal}
